42cd2f33008a816745cc90266b48210b297bbe65
limnoliver <skoliver@wisc.edu>
2016-09-23 14:02:33 -0500

    added vertical lines to denote common time measurements

--- N_retention_models.R
+++ N_retention_models.R
@@ -15,6 +15,18 @@ curve(1-(exp((-9.92*x)/10)), .001, 1000, log = "x", ylab="N Retention", xlab = "
 curve(1-(exp((-9.92*x)/20)), .001, 1000, log = "x", ylab="N Retention", xlab = "Residence Time (y)", add = TRUE)
 legend("topleft", lty = 1,lwd = 2,legend = c("5 meters", "10 meters", "20 meters"), col = c("lightblue", "blue", "black"))
 
-
+#add vertical lines to denote day, month, year, etc
+# day
+abline(v=1/365, col="red", lty=2)
+mtext("Day", side=3, outer=FALSE, at = 1/365, col="red", cex=2)
+# week
+abline(v=7/365, col="red", lty=2)
+mtext("Week", side=3, outer=FALSE, at = 7/365, col="red",cex=2)
+# month
+abline(v=30/365, col="red", lty=2)
+mtext("Month", side=3, outer=FALSE, at = 30/365, col="red",cex=2)
+# year
+abline(v=1, col = "red", lty = 2)
+mtext("Year", side=3, outer=FALSE, at = 1, col="red",cex=2)
 dev.off()
 
