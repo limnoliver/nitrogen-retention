8c72777b9e9ee25a249db6fbc26cd6d2c8d5bd3c
lukeloken <lukeloken@gmail.com>
2016-09-29 14:57:49 -0500

    Build second model that allows VF to vary.

--- N_retention_models.R
+++ N_retention_models.R
@@ -10,6 +10,8 @@ library(colorRamps)
 # z = mean depth (m)
 # Vf = settling velocity (m year-1)
 
+par(mfrow=c(1,1))
+
 # Set parameter estimates
 z = c(2, 5,10)
 Vf = c(-13.66, -9.92, -5.66) 
@@ -51,6 +53,58 @@ for (i in 1:(length(z))){
 }
 
 legend("bottomright", inset=0.02, lwd = 2,legend = paste( "z=",  table[,2], "  Vf=" ,table[,1]), col = table[,3], lty=table[,4])
+
+box(which='plot')
+
+dev.off()
+
+# ============================================
+# Same model but let Vf vary and be the x-axis
+# ============================================
+
+# Set parameter estimates
+z = c(2, 5,10)
+tau = c(1/12,1/52, 1/365) #Years
+
+vfrange<-c(-1, 3) #log scale
+
+# plot curve
+pdf("N_retention_vf.pdf")
+par(cex=1)
+
+table<-as.data.frame(matrix(ncol=4, nrow=length(Vf)*length(z)) )
+names(table)<-c("tau", "z", "color", "lty")
+
+colors<-blue2green2red(n=length(z))
+lty<-seq(1, length(tau), 1)
+
+for (i in 1:(length(z))){
+  for (j in 1:(length(tau))){
+    z1 = z[i]
+    tau1 = tau[j]
+    
+    row<-j+length(tau)*(i-1)
+    
+    table[row,1]<- tau1
+    table[row,2]<- z1
+    table[row,3]<- colors[i]
+    table[row,4]<- lty[j]
+    
+    
+    if (i ==1 & j==1){
+      # empty plot containing labels. Plot this first so vertical lines appear behind data
+      curve(1-(exp(((-1)*tau1*x)/z1)), 10^vfrange[1],10^vfrange[2], ylab="N Retention", log = "x", xlab = expression(paste(V[f], " (m year"^"-1", ")")), type="n", las=1, xaxt="n")
+      # add vertical lines to denote day, month, year, etc
+      abline(v=10^seq(vfrange[1],vfrange[2], by=1), col="darkgrey", lty=1, lwd=0.5)
+      axis(1, at=10^seq(vfrange[1],vfrange[2], by=1), labels=10^seq(-2, 2, by=1))
+
+    }
+    # Plot all curves
+    curve(1-(exp(((-1)*tau1*x)/z1)), 10^vfrange[1],10^vfrange[2], log = "x", col = colors[i], lty=lty[j], lwd=2, add=T)
+  }
+}
+
+legend("topleft", inset=0.02, lwd = 2,legend = paste( "z=",  table[,2], "m;   WRT=" ,round(table[,1]*365), " d"), col = table[,3], lty=table[,4])
 
 box(which='plot')
 
