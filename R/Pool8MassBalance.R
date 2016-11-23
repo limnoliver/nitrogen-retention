d8445e19af4517a972a78bae201f99c8585f4210
lukeloken <lukeloken@gmail.com>
2016-11-23 16:10:11 -0600

    Tried to make it plot the same size as 6 panel figure across pools

--- R/Pool8MassBalance.R
+++ R/Pool8MassBalance.R
@@ -195,28 +195,38 @@ pool_summary$Date<-as.Date(pool_summary$Date)
 pool_summary$RR_pct<-pool_summary$RR_Q/pool_summary$MR_Q
 pool_summary
 
-png("E:/Dropbox/FLAME_MississippiRiver/N_Model/N_retention_Drivers_Intrapool.png", res=200, width=5,height=5, units="in")
+png("E:/Dropbox/FLAME_MississippiRiver/N_Model/N_retention_Drivers_Intrapool.png", res=200, width=4.2,height=4, units="in")
 cex=0.8
+cexpt=1.5
 par(cex=cex)
 par(ps=12)
 
+cex=0.8
+par(cex=cex)
+cexpt=1.5
+ps=12
 par(mfrow=c(2,2))
 par(mar=c(3,1,0.5,0.5), oma=c(0,2.5,0,0))
 par(mgp=c(3,.5,0))
 par(tck=-0.03)
 par(pch=16)
 
-plot(pool_summary$Date, pool_summary$RNO3, type="b", las=1, cex.axis=cex)
+plot(pool_summary$Date, pool_summary$RNO3, type="b", las=1, cex.axis=cex, cex=cexpt)
 mtext("Date", 1, 2, cex=cex)
 abline(h=0)
-plot(pool_summary$Dam8_Q, pool_summary$RNO3, yaxt="n", cex.axis=cex)
+
+plot(pool_summary$Dam8_Q, pool_summary$RNO3, yaxt="n", cex.axis=cex, cex=cexpt)
 mtext(expression(paste("Discharge (m"^"3", " s"^"-1", ")", sep="")), 1, 2, cex=cex)
+axis(2, labels=NA)
 abline(h=0)
-plot(pool_summary$Temp, pool_summary$RNO3, yaxt="n", cex.axis=cex)
+
+plot(pool_summary$Temp, pool_summary$RNO3, cex.axis=cex, las=1, cex=cexpt)
 mtext(expression(paste("Temperature (", degree, "C)", sep="")), 1, 2, cex=cex)
 abline(h=0)
-plot(pool_summary$NO3_start, pool_summary$RNO3, yaxt="n", cex.axis=cex)
+
+plot(pool_summary$NO3_start, pool_summary$RNO3, yaxt="n", cex.axis=cex, cex=cexpt)
 mtext(expression(paste('Incoming ', NO[3], " (mg N L"^"-1", ")")),1,2, cex=cex)
+axis(2, labels=NA)
 abline(h=0)
 
 mtext(expression(paste(NO[3], ' Retention (%)')),2,1, outer=T, cex=cex)
