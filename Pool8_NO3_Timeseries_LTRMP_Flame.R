d80fececc0448cedd4696fdc8baa0e3b2e601d95
lukeloken <lukeloken@gmail.com>
2016-12-07 11:22:20 -0600

    use source function rather than copy and paste. Image.Scale

--- Pool8_NO3_Timeseries_LTRMP_Flame.R
+++ Pool8_NO3_Timeseries_LTRMP_Flame.R
@@ -50,6 +50,8 @@ df$diff<-df[,1]-df[,2]
 # hist(as.numeric(df$diff))
 mean(as.numeric(df$diff))
 
+#Plot LTRM and Discharge Data
+
 png("Figures/LTRMP_NO3_2015.png", width=5, height=3, units='in', res=600)
 
 cex=0.6
@@ -75,40 +77,8 @@ mtext("2015", 1, -.5, cex=cex, outer=T)
 
 dev.off()
 
-
-#Image Scale function for color ramps
-image.scale <- function(z, zlim, col = heat.colors(12),
-                        breaks, axis.pos=1, add.axis=TRUE, ...){
-  if(!missing(breaks)){
-    if(length(breaks) != (length(col)+1)){stop("must have one more break than colour")}
-  }
-  if(missing(breaks) & !missing(zlim)){
-    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
-  }
-  if(missing(breaks) & missing(zlim)){
-    zlim <- range(z, na.rm=TRUE)
-    zlim[2] <- zlim[2]+c(zlim[2]-zlim[1])*(1E-3)#adds a bit to the range in both directions
-    zlim[1] <- zlim[1]-c(zlim[2]-zlim[1])*(1E-3)
-    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
-  }
-  poly <- vector(mode="list", length(col))
-  for(i in seq(poly)){
-    poly[[i]] <- c(breaks[i], breaks[i+1], breaks[i+1], breaks[i])
-  }
-  if(axis.pos %in% c(1,3)){ylim<-c(0,1); xlim<-range(breaks)}
-  if(axis.pos %in% c(2,4)){ylim<-range(breaks); xlim<-c(0,1)}
-  plot(1,1,t="n",ylim=ylim, xlim=xlim, axes=FALSE, xlab="", ylab="", xaxs="i", yaxs="i", ...)  
-  for(i in seq(poly)){
-    if(axis.pos %in% c(1,3)){
-      polygon(poly[[i]], c(0,0,1,1), col=col[i], border=NA)
-    }
-    if(axis.pos %in% c(2,4)){
-      polygon(c(0,0,1,1), poly[[i]], col=col[i], border=NA)
-    }
-  }
-  box()
-  if(add.axis) {axis(axis.pos)}
-}
+# Load image scale function
+source('R/ImageScale.R')
 
 
 #get LTER chemlab data
